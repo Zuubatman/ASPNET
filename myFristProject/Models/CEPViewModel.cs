3345ff7128de8254300d21447e83238315d53c2a
Morandi <joaovitor.morandi@dellteam.com>
2024-06-26 11:45:24 -0300

    Changes in Navbar

--- myFristProject/Models/CEPViewModel.cs
+++ myFristProject/Models/CEPViewModel.cs
@@ -4,7 +4,7 @@ namespace myFristProject.Models;
 public class CEPViewModel {
 
     //Verificação de variável ( tem q importal o System.ComponentModule.DataAnnotations)
-    [StringLength ( maximumLength: 10 , ErrorMessage = "Vou dar um mortal de costa", MinimumLength = 3)]
+    [StringLength ( maximumLength: 8 , ErrorMessage = "Vou dar um mortal de costa", MinimumLength = 3)]
     public string? CEP{ get; set;}
 
     public List<string>? ceps {get; set;}
